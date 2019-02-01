84e07e61976335419c9c8f91e91a899818e08cc6
alimuratbekov <alimuratbekov@gmail.com>
2019-02-01 21:10:47 +0600

    week1

---
+++ Week1/Task3/Task3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Создаю массив
+            int n = int.Parse(Console.ReadLine());
+            int[] a = new int[n];
+            //Записываю значения в string 
+            string[] arr = Console.ReadLine().Split();
+            //Записываю массив из integer значений
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = int.Parse(arr[i]);
+            }
+            //Вывожу на экран
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(a[i] + " " + a[i] + " ");
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ Week1/Task4/Task4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ввожжу количество строк/столбцов треугольника
+            int n = int.Parse(Console.ReadLine());
+            for (int i=1;i<=n;i++)
+            {
+                //Выводит количество звездочек, равное номеру строки
+                for (int j=1;j<=i;j++)
+                {
+                    Console.Write("[*]");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
