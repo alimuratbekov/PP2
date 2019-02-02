30eb6fca2a597c2dcc26f833ed7ce1c482ec94b5
alimuratbekov <alimuratbekov@gmail.com>
2019-02-02 07:54:15 +0600

    week1

--- Week1/Task2/Task2/Program.cs
+++ Week1/Task2/Task2/Program.cs
@@ -2,11 +2,19 @@
 
 namespace Task2
 {
+    public class Student
+    {
+        public string name = "Ali";
+        public string id = "18BD110214";
+        public int year = 1;
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            
+                Student st1 = new Student();
+                Console.WriteLine(st1.name + " " + st1.id + " " + ++st1.year);
+                Console.ReadLine();
         }
     }
 }
