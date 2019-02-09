6352dd22474768b1571ba3d3b4c8d65d81236362
alimuratbekov <alimuratbekov@gmail.com>
2019-02-09 08:52:15 +0600

    week2

---
+++ Week2/Task1/Task1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace Task1
+{
+    class Program
+    {
+        //01234
+        //ababa
+        public static bool ispalin(string s)
+        {
+            for (int i = 0, j=s.Length-1; i <= j; i++, j--)
+            {
+                if (s[i] != s[j]) return false;
+            }
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader("input.txt");
+            String s = sr.ReadToEnd();
+            if (ispalin(s) == true)
+            {
+                Console.WriteLine("Yes");
+            } 
+            else
+            {
+                Console.WriteLine("No");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Week2/Task2/Task2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+namespace Task2
+{
+    class Program
+    {
+        public static bool isprime(int n)
+        {
+            for (int i=2;i<=Math.Sqrt(n);i++)
+            {
+                if (n % i == 0) return false;
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader("input.txt");
+            String s = sr.ReadToEnd();
+            String[] arr = s.Split();
+            int[] a = new int[arr.Length];
+            sr.Close();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                a[i] = int.Parse(arr[i]);
+            }
+            StreamWriter sw = new StreamWriter("output.txt");
+            for (int i=0;i<a.Length;i++)
+            {
+                if (isprime(a[i]) == true) sw.Write(a[i] + " ");
+            }
+            sw.Close();
+        }
+    }
+}
