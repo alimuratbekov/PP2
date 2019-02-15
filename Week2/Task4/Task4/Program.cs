bad6735bcc6dbae7b85135ad2ab397a222bb2f96
alimuratbekov <alimuratbekov@gmail.com>
2019-02-15 10:06:22 +0600

    new solution

---
+++ Week2/Task4/Task4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Объявляю переменную, которой присвоен путь до файла
+            string path = "/Users/aliko/desktop/MINDKILLERS/File.txt";
+            string path1 = "/Users/aliko/desktop";
+            FileInfo fi = new FileInfo(path);
+            //копирую из path в path1
+            fi.CopyTo(path1 + "/Copy.txt",true);
+            //удаляю в path
+            fi.Delete();
+        }
+    }
+}
